27238fb68f2cb288c9032bf385240be7ee264559
Yenci <a278862002@gmail.com>
2023-07-17 20:35:14 +0800

    Initial Commit

---
+++ class1/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  class1
+//
+//  Created by 陳妍慈 on 2023/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var crayon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        crayon.alpha = 1.0
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        UIView.animate(withDuration: 5.0) {
+            self.crayon.alpha = 1.0
+            self.crayon.alpha = 0.0
+        }
+    }
+    
+    
+}
+
+
